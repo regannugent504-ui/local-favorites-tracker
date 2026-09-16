16c8c0e90b2f6d7e7293fef2441eaf63ee30c213
regannugent504-ui <regannugent504@gmail.com>
2026-09-16 11:25:22 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('app.js connected');
+let myFavorite = {
+    name: 'Ampersand on University Drive',
+    category: 'coffee',
+    rating: 5,
+    notes: 'Great study spot with fast wifi',
+    dateAdded: 'May 2026'
+};
+
+console.log(myFavorite.name);
+
+let displayText = myFavorite.name + ' - Rating: ' + myFavorite.rating + '/5';
+console.log(displayText);
+
+let today = new Date().toLocaleDateString();
+console.log(today);
+
+console.log(myFavorite);
+console.log(typeof myFavorite.name);
+console.log(typeof myFavorite.rating);
