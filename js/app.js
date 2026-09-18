2cb03bb01b7cd1f40f0762bdfbf537bd4a2a70fa
regannugent504-ui <regannugent504@gmail.com>
2026-09-18 11:07:35 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,3 +17,21 @@ console.log(today);
 console.log(myFavorite);
 console.log(typeof myFavorite.name);
 console.log(typeof myFavorite.rating);
+
+function greetFavorite(placeName, rating) {
+    console.log(placeName + ' has ' + rating + ' stars!');
+}
+greetFavorite('Starbucks', 5);   // "Starbucks has 5 stars!"
+
+const nameInput = document.getElementById('name');
+console.log(nameInput.value);   // what the user typed
+
+const practiceForm = document.getElementById('add-favorite-form');
+
+function handleSubmit(event) {
+    event.preventDefault();   // stop the page reload
+    console.log('You typed: ' + nameInput.value);
+}
+
+practiceForm.addEventListener('submit', handleSubmit);
+
